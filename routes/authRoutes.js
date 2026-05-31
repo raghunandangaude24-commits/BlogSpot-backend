76d2cf5e7284e60f4a91ebc2d765d6439e3b147a
raghunandangaude24-commits <raghunandangaude24@gmail.com>
2026-05-31 18:32:09 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,14 +1,43 @@
-const express = require("express");
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-const router = express.Router();
+const registerUser = async (req, res) => {
+    try {
+        const { username, email, password } = req.body;
 
-const {
-    registerUser,
-    loginUser
-} = require("../controllers/authController");
+        console.log("REGISTER BODY:", req.body);
 
-router.post("/register", registerUser);
+        if (!username || !email || !password) {
+            return res.status(400).json({
+                message: "All fields are required"
+            });
+        }
 
-router.post("/login", loginUser);
+        const existingUser = await User.findOne({ email });
 
-module.exports = router;
+        if (existingUser) {
+            return res.status(400).json({
+                message: "User already exists"
+            });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = await User.create({
+            username,
+            email,
+            password: hashedPassword
+        });
+
+        res.status(201).json({
+            message: "User registered successfully"
+        });
+
+    } catch (error) {
+        console.log("REGISTER ERROR:", error);
+        res.status(500).json({
+            message: "Server Error"
+        });
+    }
+};
